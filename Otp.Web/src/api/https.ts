e88cff5bc82422d3cb7411d213d138a621adb361
SeanErvinson <ervinsonong@gmail.com>
2022-01-08 21:52:22 +0800

    Fix expired token  by acquiring new token redirect

--- Otp.Web/src/api/https.ts
+++ Otp.Web/src/api/https.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 import msalClient from '@/services/auth/msalClient';
+import { InteractionRequiredAuthError } from '@azure/msal-browser';
 
 const instance = axios.create({
 	baseURL: import.meta.env.VITE_OTP_API_BASE_URL,
@@ -8,28 +9,38 @@ const instance = axios.create({
 
 instance.interceptors.request.use(async config => {
 	var token = await acquireAccessToken();
-	config.headers = {
-		Authorization: `Bearer ${token}`,
-		Accept: 'application/json',
-		'Content-Type': 'application/json;charset=UTF-8',
-	};
+
+	if (!!token) {
+		config.headers = {
+			Authorization: `Bearer ${token}`,
+			Accept: 'application/json',
+			'Content-Type': 'application/json;charset=UTF-8',
+		};
+	}
 
 	return config;
 });
 
 export default instance;
 
-const acquireAccessToken = async () => {
+const acquireAccessToken = async (): Promise<string | null> => {
 	const activeAccount = msalClient.getActiveAccount();
 	const accounts = msalClient.getAllAccounts();
 	if (!activeAccount && accounts.length === 0) {
+		console.log('No account');
 	}
 
 	const request = {
 		scopes: ['https://otpdev.onmicrosoft.com/api/access_as_user'],
 		account: activeAccount || accounts[0],
 	};
-	const authResult = await msalClient.acquireTokenSilent(request);
-
-	return authResult.accessToken;
+	try {
+		const authResult = await msalClient.acquireTokenSilent(request);
+		return authResult.accessToken;
+	} catch (error) {
+		if (error instanceof InteractionRequiredAuthError) {
+			msalClient.acquireTokenRedirect(request);
+		}
+	}
+	return null;
 };
