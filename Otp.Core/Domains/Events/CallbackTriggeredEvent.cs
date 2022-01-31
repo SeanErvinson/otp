416e6f268010ab37c3a7a21d50a23ba51e693e96
SeanErvinson <ervinsonong@gmail.com>
2022-01-31 22:14:13 +0800

    Added event for callback and otp request

---
+++ Otp.Core/Domains/Events/CallbackTriggeredEvent.cs
@@ -0,0 +1,18 @@
+﻿using Otp.Core.Domains.Common;
+using Otp.Core.Domains.Entities;
+
+namespace Otp.Core.Domains.Events;
+
+public class CallbackTriggeredEvent : DomainEvent
+{
+	public CallbackTriggeredEvent(CallbackEvent callbackEvent, string callbackUrl, string? endpointSecret)
+	{
+		CallbackEvent = callbackEvent;
+		CallbackUrl = callbackUrl;
+		EndpointSecret = endpointSecret;
+	}
+
+	public CallbackEvent CallbackEvent { get; }
+	public string CallbackUrl { get; }
+	public string? EndpointSecret { get; }
+}
---
+++ Otp.Core/Domains/Events/OtpRequestedEvent.cs
@@ -0,0 +1,14 @@
+﻿using Otp.Core.Domains.Common;
+using Otp.Core.Domains.Entities;
+
+namespace Otp.Core.Domains.Events;
+
+public class OtpRequestedEvent : DomainEvent
+{
+	public OtpRequest OtpRequest { get; }
+
+	public OtpRequestedEvent(OtpRequest otpRequest)
+	{
+		OtpRequest = otpRequest;
+	}
+}
