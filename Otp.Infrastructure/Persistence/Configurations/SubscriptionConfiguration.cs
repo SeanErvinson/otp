af98d9d0a92162f610133c4de7f34cb2f916c773
SeanErvinson <ervinsonong@gmail.com>
2022-02-06 13:11:48 +0800

    Added subscription and discount table

---
+++ Otp.Infrastructure/Persistence/Configurations/SubscriptionConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Otp.Core.Domains.Entities;
+
+namespace Otp.Infrastructure.Persistence.Configurations;
+
+public class SubscriptionConfiguration : BaseEntityConfiguration<Subscription>
+{
+	public override void Configure(EntityTypeBuilder<Subscription> builder)
+	{
+		base.Configure(builder);
+		builder.ToTable("Subscriptions", tableBuilder =>
+		{
+			tableBuilder.IsTemporal();
+		});
+		builder.Property(property => property.Plan).HasConversion<string>();
+	}
+}
