217a70f20c8ed2d0ff046f32251778e9a65c86fa
Sean <ervinsonong@gmail.com>
2022-02-22 22:28:35 +0800

    Updated packages and fix drawer

--- Otp.Web/src/components/Sidebar/Sidebar.tsx
+++ Otp.Web/src/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,7 @@
-import { FunctionComponent, ReactChild, ReactChildren, ReactNode } from 'react';
+import { FunctionComponent, ReactChildren, ReactNode } from 'react';
 import { NavLink } from 'react-router-dom';
+
+import MenuIcon from '@/components/misc/MenuIcon';
 
 interface Props {
 	children: ReactChildren | ReactNode;
@@ -7,12 +9,18 @@ interface Props {
 
 const Sidebar: FunctionComponent<Props> = props => {
 	return (
-		<div id="sidebar" className="shadow bg-base-200 drawer drawer-mobile h-full">
-			<input id="my-drawer-2" type="checkbox" className="drawer-toggle" />{' '}
-			{/* TODO: Rename id and add a hamburger when in mobile */}
-			<div className="drawer-content h-full w-full p-8">{props.children}</div>
+		<div id="sidebar" className="shadow bg-base-200 h-full drawer drawer-mobile w-full">
+			<input id="drawer" type="checkbox" className="drawer-toggle" />
+			<div className="drawer-content h-full w-full">
+				<div className="w-full navbar lg:hidden">
+					<label htmlFor="drawer" className="btn btn-square btn-ghost">
+						<MenuIcon className="stroke-none fill-neutral h-10" />
+					</label>
+				</div>
+				<div className="px-4 lg:p-6">{props.children}</div>
+			</div>
 			<div className="drawer-side">
-				<label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+				<label htmlFor="drawer" className="drawer-overlay"></label>
 				<ul className="menu p-3 text-md overflow-y-auto w-56 bg-base-100 text-base-content">
 					<li>
 						<NavLink to="/">Home</NavLink>
