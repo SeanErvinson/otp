1f0c898856c2553bf5cb4fd4076ced1da8455d73
Sean <ervinsonong@gmail.com>
2022-04-29 19:19:15 +0800

    Include docsaurus gitignore

--- Otp.Site/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/home/asean/RiderProjects/Otp/Otp.Site/node_modules/infima/dist/css/default/default.css'),
-  require('/home/asean/RiderProjects/Otp/Otp.Site/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/asean/RiderProjects/Otp/Otp.Site/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
-  require('/home/asean/RiderProjects/Otp/Otp.Site/src/css/custom.css'),
-];
