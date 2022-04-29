604bc8d7040f194301ab52feb1db614f38d2c74e
Sean <ervinsonong@gmail.com>
2022-04-29 22:11:30 +0800

    added ajax submition

--- Otp.Site/src/pages/contact.tsx
+++ Otp.Site/src/pages/contact.tsx
@@ -1,7 +1,21 @@
 import Layout from "@theme/Layout";
-import React from "react";
+import React, { FormEvent } from "react";
 
 const Contact = () => {
+  const handleOnSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(event.currentTarget);
+    fetch("/contact", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(
+        new FormData(event.currentTarget) as any
+      ).toString(),
+    })
+      .then(() => console.log("Form successfully submitted"))
+      .catch((error) => alert(error));
+  };
+
   return (
     <Layout title="Contact Us" description="Very simple and affordable">
       <div className="container px-6 py-16 mx-auto text-center">
@@ -14,6 +28,14 @@ const Contact = () => {
           </p>
         </div>
       </div>
+      <form name="newsletter" data-netlify="true" onSubmit={handleOnSubmit}>
+        <input type="hidden" name="form-name" value="newsletter" />
+        <p>
+          <label>
+            Email <input type="email" name="email" />
+          </label>
+        </p>
+      </form>
     </Layout>
   );
 };
