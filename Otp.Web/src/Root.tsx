38a84846a054e9416b28a0f295e3c8aacdf6e134
Sean <ervinsonong@gmail.com>
2022-06-24 14:56:15 +0800

    Fix route paths

--- Otp.Web/src/Root.tsx
+++ Otp.Web/src/Root.tsx
@@ -9,8 +9,8 @@ import Billing from '@/modules/Billing/views/Billing';
 import Channel from '@/modules/Channel/views/Channel';
 import Logs from '@/modules/Logs/views/Logs';
 import Apps from '@/modules/Apps/views/Apps';
-import AppDetails from '@/modules/Apps/views/Apps';
-import Settings from '@/modules/Apps/views/Apps';
+import AppDetails from '@/modules/Apps/views/AppDetails';
+import Settings from '@/modules/Apps/views/Settings/Settings';
 import NotFound from '@/modules/common/views/NotFound';
 import { RecentCallbacks } from '@/modules/RecentCallbacks';
 import { loginRequest } from '@/services/auth/authConfig';
@@ -40,10 +40,12 @@ const Root = () => {
 		<Routes>
 			<Route path="/" element={<SidebarLayout />}>
 				<Route path="" element={<Dashboard />} />
-				<Route path="apps" element={<Apps />} />
-				<Route path="apps/:appId" element={<AppDetails />}>
-					<Route path="" element={<Settings />} />
-					<Route path="recent-callbacks" element={<RecentCallbacks />} />
+				<Route path="apps">
+					<Route path="" element={<Apps />} />
+					<Route path=":appId" element={<AppDetails />}>
+						<Route path="" element={<Settings />} />
+						<Route path="recent-callbacks" element={<RecentCallbacks />} />
+					</Route>
 				</Route>
 				<Route path="billing" element={<Billing />} />
 				<Route path="logs" element={<Logs />} />
