9d1e3b3c5175c439e228ab7a705d06d72f230985
Sean Ervinson <ervinsonong@gmail.com>
2022-09-18 20:48:04 +0800

    Test programmatically invoking grecaptcha

--- Otp.Site/src/components/NewsletterCTA/index.tsx
+++ Otp.Site/src/components/NewsletterCTA/index.tsx
@@ -1,27 +1,39 @@
-import React, { FormEvent } from 'react';
+import React, { FormEvent, useState } from 'react';
 
 declare var grecaptcha: any;
 
-const handleOnSubmit = (event: FormEvent<HTMLFormElement>) => {
-	event.preventDefault();
-	var response = grecaptcha.getResponse();
-
-	if (response.length == 0) {
-		fetch('/', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-			body: new URLSearchParams(
-				new FormData({ ...event.currentTarget, 'g-recaptcha-response': response }) as any,
-			).toString(),
-		})
-			.then(() => console.log('Form successfully submitted'))
-			.catch(error => alert(error));
-	} else {
-		alert('Failed to validate recaptcha');
-	}
-};
-
 const NewsletterCTA = () => {
+	const handleOnSubmit = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		setFormData(null);
+		console.log('Executing grecaptcha');
+
+		grecaptcha.execute();
+
+		console.log('Executed grecaptcha');
+
+		setFormData(event.currentTarget);
+	};
+
+	const onSubmit = (token: string) => {
+		console.log('Executing callback');
+		console.log(token);
+
+		if (formData) {
+			fetch('/', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+				body: new URLSearchParams(
+					new FormData({ ...formData, 'g-recaptcha-response': token }) as any,
+				).toString(),
+			})
+				.then(() => console.log('Form successfully submitted'))
+				.catch(error => alert(error));
+		}
+	};
+
+	const [formData, setFormData] = useState<HTMLFormElement | null>();
+
 	return (
 		<>
 			<div className="w-full mt-8 bg-transparent border rounded-md lg:max-w-sm dark:border-gray-700 focus-within:border-blue-400 focus-within:ring focus-within:ring-blue-300 dark:focus-within:border-blue-400 focus-within:ring-opacity-40">
@@ -41,11 +53,12 @@ const NewsletterCTA = () => {
 					/>
 					<div
 						className="g-recaptcha"
+						data-callback="onSubmit"
 						data-sitekey="6LdI3f8hAAAAAO_5fv1tetK__ZnCL0X2j-kDsCu-"
 						data-size="invisible"></div>
 					<button
 						type="submit"
-						className="g-recaptcha h-10 px-4 py-2 m-1 text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:bg-blue-400">
+						className="h-10 px-4 py-2 m-1 text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:bg-blue-400">
 						Get Notified
 					</button>
 				</form>
