409fc7a76710362619e3bee53d91ef1bc11afee5
Sean Ervinson <ervinsonong@gmail.com>
2022-09-25 20:37:27 +0800

    Update DI

--- Otp.Application/DependencyInjection.cs
+++ Otp.Application/DependencyInjection.cs
@@ -6,16 +6,18 @@ using MediatR;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Otp.Application.Consumers;
+using Otp.Application.PipelineBehaviors;
 
 namespace Otp.Application;
 
 public static class DependencyInjection
 {
 	public static void AddApplication(this IServiceCollection services, IConfiguration configuration)
-	{
-		services.AddMediatR(Assembly.GetExecutingAssembly());
+	{ 
+		services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
 		services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
-
+		services.AddMediatR(Assembly.GetExecutingAssembly());
+		
 		services.AddMassTransit(configurator =>
 		{
 			configurator.SetKebabCaseEndpointNameFormatter();
