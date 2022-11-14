5dce6dc72b64a0f0e460ece95e9573cc3bd66be3
Sean Ervinson <ervinsonong@gmail.com>
2022-11-14 20:05:39 +0800

    Added icon to sidebar

---
+++ Otp.Web/src/components/misc/DashboardIcon.tsx
@@ -0,0 +1,10 @@
+import { SVGProps, memo } from 'react';
+
+const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
+	<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
+		<path d="M4 13h6a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1zm-1 7a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-4a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v4zm10 0a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-7a1 1 0 0 0-1-1h-6a1 1 0 0 0-1 1v7zm1-10h6a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1h-6a1 1 0 0 0-1 1v5a1 1 0 0 0 1 1z" />
+	</svg>
+);
+
+const Memo = memo(SvgComponent);
+export default Memo;
